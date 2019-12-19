9d5266d92a9cf980fa9917c3fd279a37c137c7d9
潘赵 <5489043+pan_zhao@user.noreply.gitee.com>
2019-12-19 11:36:24 +0800

    项目模板完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 直接导入plugins/element.js
+
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
---
+++ src/plugins/element.js
@@ -0,0 +1,8 @@
+// 导入vue
+import Vue from 'vue'
+// 从elemet-ui这个里面按需导入你所需要的组件
+import { Button, Select } from 'element-ui'
+
+// 通过vue.use() 挂载所导入的组件
+Vue.use(Button)
+Vue.use(Select)
