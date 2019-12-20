4fb2b56413f7764ab2512d56b3bcde7a2fca2d53
潘赵 <5489043+pan_zhao@user.noreply.gitee.com>
2019-12-20 15:14:03 +0800

    完成登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,18 @@
 // 导入vue
 import Vue from 'vue'
 // 从elemet-ui这个里面按需导入你所需要的组件
-import { Button, Select } from 'element-ui'
-
+import {
+  Button,
+  Select,
+  Form,
+  FormItem,
+  Input,
+  Message
+} from 'element-ui'
 // 通过vue.use() 挂载所导入的组件
 Vue.use(Button)
 Vue.use(Select)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
